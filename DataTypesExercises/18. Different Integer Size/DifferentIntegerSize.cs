eb017d6e2ae120e202c067ce09a4c296fee0b5b3
VenciSarov <venci.sarov@gmail.com>
2017-01-20 17:50:41 +0200

    New solution added

---
+++ DataTypesExercises/18. Different Integer Size/DifferentIntegerSize.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Numerics;
+class DifferentIntegerSize
+{
+    static void Main()
+    {
+        PrintTypesWhichCatFit();
+    }
+
+    static void PrintTypesWhichCatFit()
+    {
+        BigInteger inputNum = BigInteger.Parse(Console.ReadLine());
+
+        if (inputNum <= long.MaxValue)
+        {
+            Console.WriteLine($"{inputNum} can fit in:");
+
+            if (inputNum >= sbyte.MinValue && inputNum <= sbyte.MaxValue)
+                Console.WriteLine("* sbyte");
+            if (inputNum >= byte.MinValue && inputNum <= byte.MaxValue)           
+                Console.WriteLine("* byte");
+            if (inputNum >= short.MinValue && inputNum <= short.MaxValue)           
+                Console.WriteLine("* short");
+            if (inputNum >= ushort.MinValue && inputNum <= ushort.MaxValue)          
+                Console.WriteLine("* ushort");
+            if (inputNum >= int.MinValue && inputNum <= int.MaxValue)            
+                Console.WriteLine("* int");
+            if (inputNum >= uint.MinValue && inputNum <= uint.MaxValue)        
+                Console.WriteLine("* uint");
+            if (inputNum >= long.MinValue && inputNum <= long.MaxValue)            
+                Console.WriteLine("* long");             
+        }
+        else
+            Console.WriteLine($"{inputNum} can't fit in any type");
+    }
+}
+
